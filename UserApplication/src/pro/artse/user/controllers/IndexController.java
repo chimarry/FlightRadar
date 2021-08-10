0baab447ef6745d8b9213c8000f63825b147df71
Marija Novakovic <marija.vanja.novakovic@gmail.com>
2021-08-10 12:33:20 +0200

    Add sceleton for pages and write Hello world

---
+++ UserApplication/src/pro/artse/user/controllers/IndexController.java
@@ -0,0 +1,30 @@
+package pro.artse.user.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/IndexController")
+public class IndexController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public IndexController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

a6a9f62ea18aa7b28c076dbb1b31d9cf81b80e97
Marija Novakovic <marija.vanja.novakovic@gmail.com>
2021-08-22 17:46:25 +0200

    Save changes

--- UserApplication/src/pro/artse/user/controllers/RegistrationController.java
+++ UserApplication/src/pro/artse/user/controllers/RegistrationController.java
@@ -8,8 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import pro.artse.dal.dto.AccountDTO;
-import pro.artse.dal.dto.AccountRole;
 import pro.artse.dal.dto.UserDTO;
 import pro.artse.dal.errorhandling.DbResultMessage;
 import pro.artse.dal.services.IAccountService;

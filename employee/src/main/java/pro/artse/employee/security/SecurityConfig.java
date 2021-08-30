9eb5302da78b079aed859d3b394a55b0d90ab72d
Marija Novakovic <marija.vanja.novakovic@gmail.com>
2021-08-30 18:20:38 +0200

    Add packages to spring boot project

---
+++ employee/src/main/java/pro/artse/employee/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package pro.artse.employee.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
+    	http.authorizeRequests().anyRequest().permitAll();
+    }
+
+}

64dc34a18b6d45af5da5cd5f02f5de648853570e
Marija Novakovic <marija.vanja.novakovic@gmail.com>
2021-08-30 18:10:45 +0200

    Add spring boot project

---
+++ employee/src/test/java/pro/artse/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.artse.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
